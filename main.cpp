ab73138032c7339a00caa22a5be50f996934bde2
royrojas07 <38670379+royrojas07@users.noreply.github.com>
2018-10-25 07:43:22 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,320 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: alan.calderon
+ *
+ * Created on October 22, 2018, 6:00 PM
+ */
+
+#include <iostream>
+#include <exception>
+using namespace std;
+
+#include "matriz_rala.h"
+#include "itr_matriz_rala.h"
+#include "itr_matriz_rala.h"
+
+
+// "%TEST_FAILED% testname = testMatriz_rala() message = "
+
+void testMatriz_rala() { // pruebas sobre constructor estándar
+    // detecta dimensiones incorrectas:
+    matriz_rala< int > mr; // se crea una matriz_rala de 20x10
+    if (mr.obtCF() != 20 || mr.obtCC() != 10) {
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala() message = dimensiones diferentes de 20x10" << std::endl;
+    }
+
+    // detecta contenido incorrecto, algún elemento diferente de cero
+    const matriz_rala< int >& mr_alias = mr; // se crea una referencia const para poder invocar los operator[] const
+    bool no_cero = true;
+    for (int i = 0; i < 20; i++)
+        for (int j = 0; j < 10; j++)
+            if (0 != mr_alias[i][j])
+                no_cero = false;
+    if (!no_cero)
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala() message = no todos los elementos son cero" << std::endl;
+
+
+    // pruebas de generación de excepciones:
+    bool detecto_excepcion = false;
+    try {
+        mr[0][0] = 1;
+    } catch (out_of_range oor) {
+        detecto_excepcion = true;
+    }
+    if (!detecto_excepcion)
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala() message = no detecto excepcion en mr[0][0] = 1;" << std::endl;
+    else detecto_excepcion = false;
+
+    try {
+        mr[20][0] = 1;
+    } catch (out_of_range oor) {
+        detecto_excepcion = true;
+    }
+    if (!detecto_excepcion)
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala() message = no detecto excepcion en mr[20][0] = 1;" << std::endl;
+    else detecto_excepcion = false;
+
+    try {
+        mr[0][10] = 1;
+    } catch (out_of_range oor) {
+        detecto_excepcion = true;
+    }
+    if (!detecto_excepcion)
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala() message = no detecto excepcion en mr[0][10] = 1;" << std::endl;
+    else detecto_excepcion = false;
+}
+
+void testMatriz_rala2() {
+    matriz_rala< int > mr(5, 10); // crea una matriz_rala de 5x10
+
+    // detecta dimensiones incorrectas:
+    if (mr.obtCF() != 5 || mr.obtCC() != 10) {
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala2() message = dimensiones diferentes de 5x10" << std::endl;
+    }
+
+    // detecta contenido incorrecto, algún elemento diferente de cero
+    const matriz_rala< int >& mr_alias = mr; // se crea una referencia const para poder invocar los operator[] const
+    bool no_cero = true;
+    for (int i = 0; i < 5; i++)
+        for (int j = 0; j < 10; j++)
+            if (mr_alias[i][j] != 0)
+                no_cero = false;
+    if (!no_cero) {
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala2() message = no todos los elementos son cero" << std::endl;
+    }
+
+    // pruebas de generación de excepciones:
+    bool detecto_excepcion = false;
+    try {
+        mr[0][0] = 1;
+    } catch (out_of_range oor) {
+        detecto_excepcion = true;
+    }
+    if (!detecto_excepcion)
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala() message = no detecto excepcion en mr[0][0] = 1;" << std::endl;
+    else detecto_excepcion = false;
+
+    try {
+        mr[0][0] = 1;
+    } catch (out_of_range oor) {
+        detecto_excepcion = true;
+    }
+    if (!detecto_excepcion)
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala() message = no detecto excepcion en mr[20][0] = 1;" << std::endl;
+    else detecto_excepcion = false;
+
+    try {
+        mr[0][10] = 1;
+    } catch (out_of_range oor) {
+        detecto_excepcion = true;
+    }
+    if (!detecto_excepcion)
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala() message = no detecto excepcion en mr[0][10] = 1;" << std::endl;
+    else detecto_excepcion = false;
+}
+
+void testMatriz_rala3() {
+    matriz_rala< int > mr_orig;
+    matriz_rala< int > mr(mr_orig);
+
+    // detecta dimensiones incorrectas:
+    if (mr.obtCF() != mr_orig.obtCF() || mr.obtCC() != mr_orig.obtCC()) {
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala3() message = dimensiones diferentes de matriz original" << std::endl;
+    }
+
+    // detecta contenido incorrecto, algún elemento diferente de cero
+    const matriz_rala< int >& mr_alias = mr; // se crea una referencia const para poder invocar los operator[] const
+    bool no_cero = true;
+    for (int i = 0; i < 20; i++)
+        for (int j = 0; j < 10; j++)
+            if (mr_alias[i][j] != 0)
+                no_cero = false;
+    if (!no_cero) {
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala3() message = no todos los elementos son cero" << std::endl;
+    }
+
+    // pruebas de generación de excepciones:
+    bool detecto_excepcion = false;
+    try {
+        mr[0][0] = 1;
+    } catch (out_of_range oor) {
+        detecto_excepcion = true;
+    }
+    if (!detecto_excepcion)
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala() message = no detecto excepcion en mr[0][0] = 1;" << std::endl;
+    else detecto_excepcion = false;
+
+    try {
+        mr[20][0] = 1;
+    } catch (out_of_range oor) {
+        detecto_excepcion = true;
+    }
+    if (!detecto_excepcion)
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala() message = no detecto excepcion en mr[20][0] = 1;" << std::endl;
+    else detecto_excepcion = false;
+
+    try {
+        mr[0][10] = 1;
+    } catch (out_of_range oor) {
+        detecto_excepcion = true;
+    }
+    if (!detecto_excepcion)
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala() message = no detecto excepcion en mr[0][10] = 1;" << std::endl;
+    else detecto_excepcion = false;
+}
+
+void testMatriz_rala_insertar() {
+    matriz_rala< int > mr; // se crea una matriz_rala de 20x10
+
+    // se agregan elementos mediante fila_rala< int >::insertar(...) y se comprueba su correcto almacenamiento:
+    try {
+        mr[0].insertar(0, 1);
+    } catch (out_of_range oor) {
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala_insertar() message = excepcion al intentar mr[0].insertar(0, 1)" << std::endl;
+    }
+
+    try {
+        mr[19].insertar(9, 99);
+    } catch (out_of_range oor) {
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala_insertar() message = excepcion al intentar mr[19].insertar(9, 99)" << std::endl;
+    }
+
+    try {
+        mr[0].insertar(5, 55);
+    } catch (out_of_range oor) {
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala_insertar() message = excepcion al intentar mr[0].insertar(5, 55)" << std::endl;
+    }
+
+    try {
+        mr[19].insertar(5, 595);
+    } catch (out_of_range oor) {
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala_insertar() message = excepcion al intentar mr[19].insertar(5, 595)" << std::endl;
+    }
+
+    // verificación del correcto almacenamiento:
+    const matriz_rala< int > mr_alias = mr; // se crea una constante
+    try {
+        if (mr_alias[0][0] != 1)
+            std::cout << "%TEST_FAILED% testname = testMatriz_rala_insertar() message = mr[0][0] != 1" << std::endl;
+    } catch (out_of_range oor) {
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala_insertar() message = excepcion al intentar mr[0][0] != 1" << std::endl;
+    }
+
+    try {
+        if (mr_alias[19][9] != 99)
+            std::cout << "%TEST_FAILED% testname = testMatriz_rala_insertar() message = mr_alias[19][9] != 99" << std::endl;
+    } catch (out_of_range oor) {
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala_insertar() message = excepcion al intentar mr_alias[19][9] != 99" << std::endl;
+    }
+
+    try {
+        if (mr_alias[0][5] != 55)
+            std::cout << "%TEST_FAILED% testname = testMatriz_rala_insertar() message = mr_alias[0][5] != 55" << std::endl;
+    } catch (out_of_range oor) {
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala_insertar() message = excepcion al intentar mr_alias[0][5] != 55" << std::endl;
+    }
+
+    try {
+        if (mr_alias[19][5] != 595)
+            std::cout << "%TEST_FAILED% testname = testMatriz_rala_insertar() message = mr_alias[19][5] != 595" << std::endl;
+    } catch (out_of_range oor) {
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala_insertar() message = excepcion al intentar mr_alias[19][5] != 595" << std::endl;
+    }
+
+    // detecta contenido incorrecto en todos los demás elementos de la matriz
+    bool no_cero = true;
+    for (int i = 0; i < 20; i++)
+        for (int j = 0; j < 10; j++)
+            if (mr_alias[i][j] != 0)
+                if (((i == 0) && (j == 0)) || ((i == 0) && (j == 5)) || ((i == 19) && (j == 5)) || ((i == 19) && (j == 9)))
+                    no_cero = true;
+                else no_cero = false;
+    if (!no_cero) {
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala3() message = no todos los elementos son cero" << std::endl;
+    }
+}
+
+void testItr_matriz_rala() {
+    matriz_rala< int > mr; // se crea una matriz_rala de 20x10
+
+    // se agregan elementos mediante fila_rala< int >::insertar(...) y se comprueba su correcto almacenamiento:
+    try {
+        mr[0].insertar(0, 1);
+    } catch (out_of_range oor) {
+        std::cout << "%TEST_FAILED% testname = testItr_matriz_rala() message = excepcion al intentar mr[0].insertar(0, 1)" << std::endl;
+    }
+
+    try {
+        mr[19].insertar(9, 99);
+    } catch (out_of_range oor) {
+        std::cout << "%TEST_FAILED% testname = testItr_matriz_rala() message = excepcion al intentar mr[19].insertar(9, 99)" << std::endl;
+    }
+
+    try {
+        mr[0].insertar(5, 55);
+    } catch (out_of_range oor) {
+        std::cout << "%TEST_FAILED% testname = testItr_matriz_rala() message = excepcion al intentar mr[0].insertar(5, 55)" << std::endl;
+    }
+
+    try {
+        mr[19].insertar(5, 595);
+    } catch (out_of_range oor) {
+        std::cout << "%TEST_FAILED% testname = testItr_matriz_rala() message = excepcion al intentar mr[19].insertar(5, 595)" << std::endl;
+    }
+
+    cout << "valores originales de la matriz: " << endl;
+    for (matriz_rala< int >::const_iterator itr = mr.begin(); itr != mr.end(); ++itr)
+        cout << "fila: " << itr.obtFila() << " columna: " << itr.obtColumna() << " valor: " << *itr << endl;
+
+    // se modifican los valores agregados:
+    try {
+        mr[0][0] = 11;
+        mr[0][5] = 66;
+        mr[19][5] = 696;
+        mr[19][9] = 88;
+    } catch (out_of_range oor) {
+        std::cout << "%TEST_FAILED% testname = testMatriz_rala_insertar() message = excepcion al intentar modificar --> " << oor.what() << std::endl;
+    }
+
+    const matriz_rala< int >& mr_alias = mr; // se crea una referencia const para invocar los observadores
+    if ((mr_alias[0][0] != 11)||(mr_alias[0][5] != 66)||(mr_alias[19][5] != 696)||(mr_alias[19][9] != 88))
+        std::cout << "%TEST_FAILED% testname = testItr_matriz_rala() message = falló alguna modificación" << std::endl;
+        
+    cout << "valores modificados de la matriz: " << endl;
+    for (matriz_rala< int >::const_iterator itr = mr.begin(); itr != mr.end(); ++itr)
+        cout << "fila: " << itr.obtFila() << " columna: " << itr.obtColumna() << " valor: " << *itr << endl;
+}
+
+int main(int argc, char** argv) {
+    std::cout << "%SUITE_STARTED% pruebas" << std::endl;
+
+    std::cout << "%TEST_STARTED% testMatriz_rala" << std::endl;
+    testMatriz_rala();
+    std::cout << "%TEST_FINISHED% testMatriz_rala" << std::endl;
+
+    std::cout << "%TEST_STARTED% testMatriz_rala2" << std::endl;
+    testMatriz_rala2();
+    std::cout << "%TEST_FINISHED% testMatriz_rala2" << std::endl;
+
+    std::cout << "%TEST_STARTED% testMatriz_rala3" << std::endl;
+    testMatriz_rala3();
+    std::cout << "%TEST_FINISHED% testMatriz_rala3" << std::endl;
+
+    std::cout << "%TEST_STARTED% testMatriz_rala_insertar" << std::endl;
+    testMatriz_rala_insertar();
+    std::cout << "%TEST_FINISHED% testMatriz_rala_insertar" << std::endl;
+
+    std::cout << "%TEST_STARTED% testItr_matriz_rala" << std::endl;
+    testItr_matriz_rala();
+    std::cout << "%TEST_FINISHED% testItr_matriz_rala" << std::endl;
+
+    std::cout << "%SUITE_FINISHED% pruebas" << std::endl;
+
+    return (EXIT_SUCCESS);
+}
---
+++ matriz_rala.h
@@ -0,0 +1,126 @@
+#pragma once
+#include <stdexcept> // para usar excepciones de tipo "out_of_range".
+#include <vector>
+using namespace std;
+
+#include "fila_rala.h"
+
+template < typename T >
+class itr_matriz_rala;
+
+template < typename T >
+class matriz_rala {
+    /* Plantilla matriz_rala: representa una matriz rala de contenido T de dimensiones cf x cc,
+     * con cf la cantidad de filas y cc la cantidad de columnas. Se pueden agregar nuevas posiciones
+     * a la matriz siempre que no se rebase ni cf ni cc. En una matriz rala se supone que la mayor
+     * parte de las entradas guardan el valor cero por lo que s�lo se guardan efectivamente los 
+     * valores diferentes de cero. 
+     * Se supone que T puede ser int, double, float, long, long int, long long int o long double.
+     */
+
+public:
+    typedef itr_matriz_rala< T > const_iterator;
+    friend class itr_matriz_rala< T >;
+
+    /* CONSTRUCTORES */
+    // Construye una matriz rala vac�a de 20 filas y 10 columnas.
+    matriz_rala();
+
+    // Construye una matriz rala vac�a de cf filas y cc columnas.
+    matriz_rala(int cf, int cc);
+
+    // Construye una matriz rala copia id�ntica a mr_orig.
+    matriz_rala(const matriz_rala< T >& mr_orig);
+
+    /* DESTRUCTOR */
+
+    ~matriz_rala();
+
+    /* OBSERVADORES */
+
+    // EFE: retorna la cantidad de filas.
+    int obtCF() const;
+
+    // EFE: retorna la cantidad de columnas.
+    int obtCC() const;
+
+
+    // EFE: retorna "out_of_range" si ( 0 > f ) || ( obtCF() <= f ), de lo contrario
+    //		retorna una copia de la fila rala correspondiente al �ndice f.
+    const fila_rala< T >& operator[](int f) const throw (out_of_range);
+
+    /* MODIFICADORES */
+    // EFE: retorna "out_of_range" si ( 0 > f ) || ( obtCF() <= f ), de lo contrario
+    //		retorna una referencia de la fila rala correspondiente al �ndice f.
+    fila_rala< T >& operator[](int f) throw (out_of_range);
+
+    /* ITERADORES */
+    // EFE: retorna un iterador que apunta a la primera posici�n de *this.
+    const_iterator& begin();
+
+    // EFE: retorna un iterador que apunta a la posici�n despu�s de la final en *this.
+    const_iterator& end();
+private:
+    int cf; // cantidad de filas
+    int cc; // cantidad de columnas
+    vector< fila_rala< T > > filas;
+
+    /* iteradores */
+    static itr_matriz_rala< T > itr_begin; // Se le asocia un CnjOrd_gnr para recorrer
+    static itr_matriz_rala< T > itr_end; // NO se le asocia nunca un CnjOrd_gnr.
+};
+
+template < typename T >
+itr_matriz_rala< T > matriz_rala< T >::itr_begin;
+
+template < typename T >
+itr_matriz_rala< T > matriz_rala< T >::itr_end;
+
+template < typename T >
+matriz_rala< T >::matriz_rala() {
+
+}
+
+template < typename T >
+matriz_rala< T >::matriz_rala(int cf, int cc) {
+
+}
+
+template < typename T >
+matriz_rala< T >::matriz_rala(const matriz_rala< T >& mr_orig) {
+
+}
+
+template < typename T >
+matriz_rala< T >::~matriz_rala() {
+}
+
+template < typename T >
+const fila_rala< T >& matriz_rala< T >::operator[](int f) const throw (out_of_range) {
+
+}
+
+template < typename T >
+fila_rala< T >& matriz_rala< T >::operator[](int f) throw (out_of_range) {
+
+}
+
+template < typename T >
+int matriz_rala< T >::obtCF() const {
+
+}
+
+template < typename T >
+int matriz_rala< T >::obtCC() const {
+
+}
+
+template < typename T >
+itr_matriz_rala< T >& matriz_rala< T >::begin() {
+
+}
+
+template < typename T >
+itr_matriz_rala< T >& matriz_rala< T >::end() {
+
+}
